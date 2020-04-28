48ec538565b963a45ba9859200b0583c54a6f8ea
amardeep <zaaiandroid@gmail.com>
2020-04-28 14:06:27 +0200

    First commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<connectivity/FLTConnectivityPlugin.h>)
+#import <connectivity/FLTConnectivityPlugin.h>
+#else
+@import connectivity;
+#endif
+
+#if __has_include(<e2e/E2EPlugin.h>)
+#import <e2e/E2EPlugin.h>
+#else
+@import e2e;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTConnectivityPlugin"]];
+  [E2EPlugin registerWithRegistrar:[registry registrarForPlugin:@"E2EPlugin"]];
+}
+
+@end
